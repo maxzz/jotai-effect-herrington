44b83e68eb1a77e007713b9ccb77b90967a8a51f
Max Zakharzhevskiy <maxzz@msn.com>
2025-02-05 16:59:24 -0800

    WIP. Code review. 3

--- src/App.tsx
+++ src/App.tsx
@@ -5,48 +5,57 @@ import { withAtomEffect } from "jotai-effect";
 import { store } from "./store";
 
 export function App() {
-
-    const [count, setCount] = useAtom(countStateAtom);
-    const doubleCount = useAtomValue(doubleCountStateAtom);
-    const [userID, setUserID] = useAtom(userIDStateAtom);
-
     return (
         <div className="text-sm flex flex-col gap-2">
             <h1 className="text-sm font-bold">Jotai</h1>
+            <Demo1 />
+            <Demo2 />
+        </div>
+    );
+}
 
-            <div className="flex items-center gap-2">
-                <button className="px-4 py-2 text-white bg-blue-500 rounded-md"
-                    onClick={() => setCount((count) => count + 1)}
-                >
-                    count is {count}
-                </button>
-                <p>double count is {doubleCount}</p>
-            </div>
+function Demo1() {
+    const [count, setCount] = useAtom(countStateAtom);
+    const doubleCount = useAtomValue(doubleCountStateAtom);
+    return (<>
+        <div className="flex items-center gap-2">
+            <button className="px-4 py-2 text-white bg-blue-500 rounded-md"
+                onClick={() => setCount((count) => count + 1)}
+            >
+                count is {count}
+            </button>
+            <p>double count is {doubleCount}</p>
+        </div>
 
-            <div>
-                <button className="px-4 py-2 text-white bg-red-500 rounded-md"
-                    onClick={() => {
-                        store.set(countStateAtom, (c) => c + 1);
-                    }}
-                >
-                    Externally Increment
-                </button>
-            </div>
+        <div>
+            <button className="px-4 py-2 text-white bg-red-500 rounded-md"
+                onClick={() => {
+                    store.set(countStateAtom, (c) => c + 1);
+                }}
+            >
+                Externally Increment
+            </button>
+        </div>
+    </>
+    );
+}
 
-            <div className="flex items-center gap-2">
-                <select className="px-4 py-2 text-gray-800 bg-gray-200  rounded-md"
-                    value={userID}
-                    onChange={(e) => setUserID(Number(e.target.value))}
-                >
-                    <option value={1}>User 1</option>
-                    <option value={2}>User 2</option>
-                    <option value={3}>User 3</option>
-                </select>
+function Demo2() {
+    const [userID, setUserID] = useAtom(userIDStateAtom);
+    return (
+        <div className="flex items-center gap-2">
+            <select className="px-4 py-2 text-gray-800 bg-gray-200  rounded-md"
+                value={userID}
+                onChange={(e) => setUserID(Number(e.target.value))}
+            >
+                <option value={1}>User 1</option>
+                <option value={2}>User 2</option>
+                <option value={3}>User 3</option>
+            </select>
 
-                <Suspense fallback={<p>Loading...</p>}>
-                    <User />
-                </Suspense>
-            </div>
+            <Suspense fallback={<p>Loading...</p>}>
+                <User />
+            </Suspense>
         </div>
     );
 }
