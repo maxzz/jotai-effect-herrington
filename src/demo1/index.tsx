1bc842221a6346345246acf1dc122960d1929b29
Max Zakharzhevskiy <maxzz@msn.com>
2025-02-06 15:02:54 -0800

    WIP. demo1. code review

--- src/demo1/index.tsx
+++ src/demo1/index.tsx
@@ -1,4 +1,4 @@
-import { type ComponentPropsWithoutRef } from "react";
+import { useEffect, type ComponentPropsWithoutRef } from "react";
 import { atom, useAtom, useAtomValue } from "jotai";
 import { classNames } from "../utils";
 
@@ -7,22 +7,29 @@ import { store } from "../store";
 export function Demo1({ className, ...rest }: ComponentPropsWithoutRef<"div">) {
     const [count, setCount] = useAtom(countStateAtom);
     const doubleCount = useAtomValue(doubleCountStateAtom);
+
+    const display = useAtomValue(displayAtom);
+
+    useEffect(
+        () => {
+            const unsub = store.sub(countStateAtom,
+                () => {
+                    const str = `countStateAtom = ${store.get(countStateAtom)}, changed from useEffect`;
+                    store.set(displayAtom, (display) => [...display, str]);
+                },
+            );
+            return unsub;
+        }, []
+    );
+
     return (
         <div className={classNames("flex flex-col gap-2", className)} {...rest}>
             <div className="flex items-center gap-2">
-                <button
-                    className="px-4 py-2 text-white bg-blue-500 rounded-md active:scale-x-[.98]"
-                    onClick={() => setCount((count) => count + 1)}
-                >
+                <button className={buttonClasses} onClick={() => setCount((count) => count + 1)} >
                     useAtom increment
                 </button>
 
-                <button
-                    className="px-4 py-2 text-white bg-red-500 rounded-md active:scale-x-[.98]"
-                    onClick={() => {
-                        store.set(countStateAtom, (c) => c + 1);
-                    }}
-                >
+                <button className={buttonClasses} onClick={() => { store.set(countStateAtom, (c) => c + 1); }} >
                     External increment
                 </button>
             </div>
@@ -30,6 +37,12 @@ export function Demo1({ className, ...rest }: ComponentPropsWithoutRef<"div">) {
             <div className="w-max grid grid-cols-[auto,auto] gap-x-2">
                 <p>count is</p> {count}
                 <p>double count is</p> {doubleCount}
+
+                <p>display is</p>
+                <div className="relative">
+                    <button className={crossButtonClasses} onClick={() => store.set(displayAtom, [])}>✖</button>
+                    <div className="whitespace-pre text-[0.65rem]">{JSON.stringify(display, null, 2)}</div>
+                </div>
             </div>
         </div>
     );
@@ -37,3 +50,15 @@ export function Demo1({ className, ...rest }: ComponentPropsWithoutRef<"div">) {
 
 const countStateAtom = atom(0);
 const doubleCountStateAtom = atom((get) => get(countStateAtom) * 2);
+
+const displayAtom = atom<string[]>([]);
+
+const unsub = store.sub(countStateAtom,
+    () => {
+        const str = `countStateAtom = ${store.get(countStateAtom)}, changed from global scope`;
+        store.set(displayAtom, (display) => [...display, str]);
+    },
+);
+
+const buttonClasses = "px-4 py-2 text-white bg-blue-500 rounded-md active:scale-x-[.98]";
+const crossButtonClasses = "absolute p-1 -top-0.5 -right-8 scale-[.7] bg-slate-700 rounded hover:bg-red-500";
